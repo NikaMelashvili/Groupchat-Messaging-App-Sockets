9189cc3aefe308f2dbf5b5093fb80d4ebdf01d4f
Nika Melashvili <109832550+NikaMelashvili@users.noreply.github.com>
2024-01-13 17:50:52 +0400

    initial upload

---
+++ PersonTwo/src/TwoP.java
@@ -0,0 +1,37 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.Scanner;
+
+public class TwoP extends Thread {
+
+    private static final int MAX_LEN = 1000;
+    private MulticastSocket socket;
+    private InetAddress group;
+    private int port;
+
+    public TwoP(MulticastSocket socket, InetAddress group, int port){
+        this.socket = socket;
+        this.port = port;
+        this.group = group;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                byte[] buffer = new byte[MAX_LEN];
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+                socket.receive(datagramPacket);
+
+                String receivedMessage = new String(buffer, 0, datagramPacket.getLength());
+                System.out.println(receivedMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ ThreeP/src/Main.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+    private static final String EXIT = "h";
+
+    public static void main(String[] args) {
+        boolean isFinished;
+        try {
+            InetAddress group = InetAddress.getByName("224.0.0.0");
+            int port = 5002;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your name:");
+            String name = scanner.nextLine();
+            MulticastSocket socket = new MulticastSocket(port);
+            socket.setTimeToLive(0);
+            socket.joinGroup(group);
+
+            ThirdP readThread = new ThirdP(socket, group, port);
+            Thread thread = new Thread(readThread);
+            thread.start();
+
+            System.out.println("type your message.");
+
+            String message;
+            while (true) {
+                message = scanner.nextLine();
+                if (message.equals(EXIT)) {
+                    isFinished = true;
+                    socket.leaveGroup(group);
+                    socket.close();
+                    break;
+                }
+                message = name + ": " + message;
+                byte[] buffer = message.getBytes();
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, group, port);
+                socket.send(datagramPacket);
+            }
+        } catch (UnknownHostException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
